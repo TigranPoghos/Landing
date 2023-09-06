489008ba826ec36e370ec9087c3cfa0897cf194d
Тигран Погосян <136622719+TigranPoghos@users.noreply.github.com>
2023-09-06 21:30:36 +0300

    js
    training

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-$('.offer__link-2').slick();
+console.log($(".allParent"))
+
+$(".child11").click(function(){
+    // $(this).parent().toggleClass("isActive")
+    // $(this).parent().next.toggleClass("isActive")
+    // $(this).parent().prev.toggleClass("isActive")
+    // $(this).closest(".allParent").toggleClass("isActive")
+    // $(this).closest(".allParent").find(".child44").toggleClass("isActive")
+    // $(this).closest(".allParent").siblings().toggleClass("isActive")
+})
